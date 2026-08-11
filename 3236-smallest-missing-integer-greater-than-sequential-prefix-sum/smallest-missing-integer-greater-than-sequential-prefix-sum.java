8b2e7d294b508f56ea1b38c3243630856201fc0c
shareefuddin886 <shareefkhaja974@gmail.com>
2026-08-11 20:07:39 +0530

    Time: 4 ms (19.94%) | Memory: 44.4 MB (23.29%) - LeetSync

--- 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.java
+++ 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.java
@@ -1,27 +1,56 @@
+// class Solution {
+//     public int missingInteger(int[] nums) {
+//         if(nums.length==1) return nums[0]+1;
+//         HashSet<Integer> set=new HashSet<>();
+//         for(int i=0;i<nums.length;i++){
+//             set.add(nums[i]);
+//         }
+//       int   sum=nums[0];
+//         for(int i=1;i<nums.length;i++){
+//             if(nums[i]==nums[i-1]+1){
+//                 sum=sum+nums[i];
+//             }
+//             else{
+//                 for(int j=0;j<nums.length;j++){
+//                     if(set.contains(sum)){
+//                         sum++;
+//                     }
+//                     else{
+//                         return sum;
+//                     }
+//                 }
+//             }
+           
+//         }
+//         return sum;
+//     }
+// }
+
+
+import java.util.Arrays;
+
 class Solution {
     public int missingInteger(int[] nums) {
-        if(nums.length==1) return nums[0]+1;
-        HashSet<Integer> set=new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-            set.add(nums[i]);
-        }
-      int   sum=nums[0];
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]==nums[i-1]+1){
-                sum=sum+nums[i];
+        // 1. Calculate prefix sum
+        int sum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1] + 1) {
+                sum += nums[i];
+            } else {
+                break;
             }
-            else{
-                for(int j=0;j<nums.length;j++){
-                    if(set.contains(sum)){
-                        sum++;
-                    }
-                    else{
-                        return sum;
-                    }
-                }
-            }
-           
         }
+        
+        // 2. Sort the array to find missing numbers sequentially
+        Arrays.sort(nums);
+        
+        // 3. Scan the sorted array to find if 'sum' exists
+        for (int num : nums) {
+            if (num == sum) {
+                sum++; // Increment if the current sum is found
+            }
+        }
+        
         return sum;
     }
 }
