2cf36357e5aeb1be80a75e71b1cdc2d0550c9a01
shareefuddin886 <shareefkhaja974@gmail.com>
2026-08-15 20:33:43 +0530

    Time: 1 ms (99.96%) | Memory: 71.1 MB (86.14%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,36 +1,22 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        Arrays.sort(nums);
+    int n=nums.length;
+        for(int i=0;i<nums.length;i++){
 
-        if (nums[0] > 1)
-            return 1;
-        if (nums.length == 1 && nums[0] == 1)
-            return 2;
-        int posindex = 0;
-        int index=0;
-        for ( index = 0; index < nums.length; index++) {
-            
-            if (nums[index] > 0) {
-                posindex = index;
-                break;
+            while(nums[i]>0 && nums[i]<=n && nums[nums[i]-1]!=nums[i]){
+                int index=nums[i]-1;
+                int temp=nums[nums[i]-1];
+                nums[nums[i]-1]=nums[i];
+                nums[i]=temp;
+
+
+
             }
-             if(index==nums.length-1) return 1;
-        }
-        if (posindex == nums.length - 1){
-            if(nums[index]==1) return 2;
-            return 1;
-        }
-        if(nums[posindex] > 1) return 1;
-       
-        for (int i = posindex; i <= nums.length - 1; i++) {
-            if (i == nums.length - 1)
-                return nums[i] + 1;
-            if(nums[i]==nums[i+1]) continue;
-            if (nums[i] + 1 != nums[i + 1])
-                return nums[i] + 1;
-
         }
 
-        return 1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=i+1) return i+1;
+        }
+        return nums[n-1]+1;
     }
 }
