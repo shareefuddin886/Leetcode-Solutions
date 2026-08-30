59da8b9298a4623ba924640b682327622f2ae8fa
shareefuddin886 <shareefkhaja974@gmail.com>
2026-08-30 14:31:10 +0530

    Time: 0 ms (100.00%) | Memory: 47.6 MB (67.90%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,27 +1,24 @@
 class Solution {
     public int trap(int[] heights) {
-        int maxleft = heights[0];
-        int[] arr1 = new int[heights.length];
-        arr1[0] = heights[0];
-
-        for (int i = 1; i < heights.length; i++) {
-            maxleft = Math.max(maxleft, heights[i]);
-
-            arr1[i] = maxleft;
+        int ans=0;
+        int n=heights.length;
+       int l=0; int r=n-1;
+        int lmax=0; int rmax=0;
+        while(l<r){
+            lmax=Math.max(lmax,heights[l]);
+            rmax=Math.max(rmax,heights[r]);
+            if(lmax<rmax){
+                ans+=(lmax-heights[l]);
+                l++;
+            }
+            else{
+                ans+=(rmax-heights[r]);
+                r--;
+            }
         }
-        int[] arr2 = new int[heights.length];
-        arr2[heights.length - 1] = heights[heights.length - 1];
-        int maxright = heights[heights.length - 1];
 
-        for (int i = heights.length - 2; i >= 0; i--) {
-            maxright = Math.max(maxright, heights[i]);
-            arr2[i] = maxright;
-        }
-        int sum = 0;
-        for (int i = 0; i < heights.length; i++) {
-            sum = sum + Math.min(arr1[i], arr2[i]) - heights[i];
-        }
-        return sum;
+        
+        return ans;
 
     }
 }
