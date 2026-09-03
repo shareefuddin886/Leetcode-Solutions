ac5f70fa250a5b8d71ef8efebd6fe2f28712c571
shareefuddin886 <shareefkhaja974@gmail.com>
2026-09-03 20:43:12 +0530

    Time: 17 ms (41.39%) | Memory: 43.8 MB (74.12%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,24 +1,31 @@
+import java.util.HashMap;
+
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if(s.length()!=t.length()) return false;
-       HashMap<Character,Character> map=new HashMap<>();
-       int i=0;int j=0;
-       while(i<s.length() && j<t.length()){
-        if(!map.containsKey(s.charAt(i))){
-            // BUG FIX: Check if the value is already mapped to a different character
-                if (map.containsValue(t.charAt(j))) {
-                    return false;
-                }
-            map.put(s.charAt(i),t.charAt(j));
-            i++;j++;
-        }
-        else{
-            if(map.get(s.charAt(i))!=t.charAt(j)) return false;
-            i++;j++;
-        }
-       }
+        if (s.length() != t.length()) return false;
         
-
-      return true;
+        HashMap<Character, Character> mapStoT = new HashMap<>();
+        HashMap<Character, Character> mapTtoS = new HashMap<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char charS = s.charAt(i);
+            char charT = t.charAt(i);
+            
+            // Check s -> t mapping
+            if (mapStoT.containsKey(charS)) {
+                if (mapStoT.get(charS) != charT) return false;
+            } else {
+                mapStoT.put(charS, charT);
+            }
+            
+            // Check t -> s mapping (replaces containsValue)
+            if (mapTtoS.containsKey(charT)) {
+                if (mapTtoS.get(charT) != charS) return false;
+            } else {
+                mapTtoS.put(charT, charS);
+            }
+        }
+        
+        return true;
     }
 }
