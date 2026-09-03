562503ed63b1851f595ba67f903bde02903308c8
shareefuddin886 <shareefkhaja974@gmail.com>
2026-09-03 10:39:02 +0530

    Time: 1 ms (99.95%) | Memory: 46.2 MB (79.49%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,32 +1,19 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character,Integer> map1=new HashMap<>();
-        HashMap<Character,Integer> map2=new HashMap<>();
-        for(int i=0;i<magazine.length();i++){
-            if(!map1.containsKey(magazine.charAt(i))){
-                map1.put(magazine.charAt(i),1);
-            }
-            else{
-                int freq=map1.get(magazine.charAt(i));
-                map1.put(magazine.charAt(i),freq+1);
-            }
-        }
-
-        for(int i=0;i<ransomNote.length();i++){
-            if(!map2.containsKey(ransomNote.charAt(i))){
-                map2.put(ransomNote.charAt(i),1);
-            }
-            else{
-                int freq=map2.get(ransomNote.charAt(i));
-                map2.put(ransomNote.charAt(i),freq+1);
-            }
-        }
-        for(char ele:map2.keySet()){
-            if(!map1.containsKey(ele)){
-                return false;
-            }
-            else if(map2.get(ele)>map1.get(ele)) return false;
-        }
+      int[] arr1=new int[26];
+      int[] arr2=new int [26];
+      for(int i=0;i<magazine.length();i++){
+        int index=magazine.charAt(i)-'a';
+        arr1[index]=arr1[index]+1;
+      }
+      for(int i=0;i<ransomNote.length();i++){
+        int index=ransomNote.charAt(i)-'a';
+        arr2[index]=arr2[index]+1;
+      }
+     
+    for(int i=0;i<26;i++){
+        if(arr2[i]>arr1[i]) return false;
+    }
         return true;
     }
 }
