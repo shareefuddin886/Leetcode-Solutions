e60d4e94675fcb1442347c5c48b87a44f0745ab4
shareefuddin886 <shareefkhaja974@gmail.com>
2026-09-05 00:06:37 +0530

    Time: 5 ms (49.49%) | Memory: 43.4 MB (53.49%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -1,36 +1,36 @@
 class Solution {
     public int maxNumberOfBalloons(String s) {
-        HashMap<Character,Integer> map=new HashMap<>();
+       int[] arr=new int[26];
         for(int i=0;i<s.length();i++){
             char ele=s.charAt(i);
             if(ele=='b'|| ele=='a' || ele=='l' || ele=='o' || ele=='n'){
-            if(!map.containsKey(ele)){
-               map.put(ele,1);
-            }
-            else{
-            int freq=map.get(ele);
+            int index=ele-'a';
+            arr[index]=arr[index]+1;
 
-            map.put(ele,freq+1);
-            }
         }
         }
+
        int b=0;int a=0; int l=0;int o=0; int n=0;
-        for(char keys:map.keySet()){
-            if(keys=='b'){
-               b= map.get(keys);
-            }
-            else if(keys=='a'){
-                a=map.get(keys);
-            }
-            else if(keys=='l'){
-                l=map.get(keys);
-            }
-            else if(keys=='o'){
-                o=map.get(keys);
-            }
-            else if(keys=='n'){
-                n=map.get(keys);
-            }
+       String name="balon";
+        for(int i=0;i<name.length();i++){
+            char ele=name.charAt(i);
+            int index=ele-'a';
+           if(ele=='b'){
+             b=arr[index];
+           }
+           else if(ele=='a'){
+            a=arr[index];
+           }
+           else if(ele=='l'){
+            l=arr[index];
+           }
+           else if(ele=='o'){
+            o=arr[index];
+           }
+           else if(ele=='n'){
+            n=arr[index];
+           }
+
         }
         if(b<1 || a<1 || l<2 || o<2 || n<1) return 0;
        int result=0;
