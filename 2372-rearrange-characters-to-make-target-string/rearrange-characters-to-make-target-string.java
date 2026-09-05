f08bac948ac790435201c008f2000b6273bfaaca
shareefuddin886 <shareefkhaja974@gmail.com>
2026-09-05 21:14:57 +0530

    Time: 3 ms (4.26%) | Memory: 43.2 MB (21.10%) - LeetSync

--- 2372-rearrange-characters-to-make-target-string/rearrange-characters-to-make-target-string.java
+++ 2372-rearrange-characters-to-make-target-string/rearrange-characters-to-make-target-string.java
@@ -1,28 +1,40 @@
 class Solution {
     public int rearrangeCharacters(String s, String target) {
               int[]  arr=new int[26];
-              int[] arr2=new int[26];
+             
               char[] str=s.toCharArray();
-              char[] tar=target.toCharArray();
+             
               for(int i=0;i<str.length;i++){
                 int index=str[i]-'a';
                 arr[index]=arr[index]+1;
               }
-               for(int i=0;i<tar.length;i++){
-                int index=tar[i]-'a';
-                arr2[index]=arr2[index]+1;
-              }
+              int count=0;
+              
+            char[] tar=target.toCharArray();
+          
+
+while (true) {
+
+    String name = "";
+
+    for (int i = 0; i < tar.length; i++) {
+
+        int index = tar[i] - 'a';
+
+        if (arr[index] == 0) {
+            return count;
+        }
+
+        arr[index]--;
+        name += tar[i];
+    }
+
+    if (name.equals(target)) {
+        count++;
+    }
+}
 
               
-
-              int result=Integer.MAX_VALUE;
-             for(int i=0;i<tar.length;i++){
-               int index=tar[i]-'a';
-               int  currmin=arr[index]/arr2[index];
-               result=Math.min(result,currmin);
-             }
-
-              return result;
               
     }
 }
