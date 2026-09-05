a8ec5263cf3749d21fe8b8b97bfc5c50b9a93c86
shareefuddin886 <shareefkhaja974@gmail.com>
2026-09-05 09:58:07 +0530

    Time: 2 ms (94.40%) | Memory: 43.5 MB (44.68%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxNumberOfBalloons(String s) {
-       int[] arr=new int[26];
+        int[] arr=new int[26];
        char[] str=s.toCharArray();
         for(int i=0;i<str.length;i++){
             char ele=str[i];
@@ -10,13 +10,40 @@ class Solution {
 
         
         }
-      int result=Integer.MAX_VALUE;
-     result=Math.min(result,arr['b'-'a']);
-     result=Math.min(result,arr['a'-'a']);
-     result=Math.min(result,arr['l'-'a']/2);
-     result=Math.min(result,arr['o'-'a']/2);
-     result=Math.min(result,arr['n'-'a']);
-
+        int a=0;
+        int b=0;
+        int l=0;
+        int o=0;
+        int n=0;
+        char[] name={'b','a','l','o','n'};
+        for(char keys:name){
+            int index=keys-'a';
+            if(keys=='b'){
+               b=arr[index];
+            }
+            else if(keys=='a'){
+               a=arr[index];
+            }
+            else if(keys=='l'){
+              l=arr[index];
+            }
+            else if(keys=='o'){
+                o=arr[index];
+            }
+            else if(keys=='n'){
+               n=arr[index];
+            }
+        }
+        if(b<1 || a<1 || l<2 || o<2 || n<1) return 0;
+       int result=0;
+       while(a!=0 && b!=0 && !(l<2) && !(o<2) && n!=0){
+        result++;
+        a--;
+        b--;
+        l=l-2;
+        o=o-2;
+        n--;
+       }
         
         
         return result;
