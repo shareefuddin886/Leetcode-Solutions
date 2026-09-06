a12b89633df0a217772eba49c08b8715ab58160f
shareefuddin886 <shareefkhaja974@gmail.com>
2026-09-06 12:04:25 +0530

    Time: 2 ms (75.11%) | Memory: 43.3 MB (27.51%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.java
+++ 409-longest-palindrome/longest-palindrome.java
@@ -1,28 +1,32 @@
 class Solution {
     public int longestPalindrome(String s) {
-       HashMap<Character,Integer> map=new HashMap<>();
-       char[] str=s.toCharArray();
-       for(int i=0;i<str.length;i++){
-        char ele=str[i];
-        map.put(ele,map.getOrDefault(ele,0)+1);
+        int[] counts = new int[52];
+        char[] str = s.toCharArray();
+        for (int i = 0; i < str.length; i++) {
+            char ch = str[i];
 
-       }
+            if (ch >= 'a' && ch <= 'z') {
+                counts[ch - 'a']++; // 0 to 25
+            } else if (ch >= 'A' && ch <= 'Z') {
+                counts[ch - 'A' + 26]++; // 26 to 51
+            }
+        }
 
-       
-       int count=0;
-       int oddcount=0;
-      for(char ele:map.keySet()){
-      int freq=map.get(ele);
-      if(freq%2==0){
-        count=count+freq;
-      }
-      
-      else{
-        count=count+freq-1;
-        oddcount++;
-      }
-      }
-      if(oddcount>0) return count+1;
-      return count;
+        int count = 0;
+        int oddcount = 0;
+        for (int ele :counts) {
+            int freq = ele;
+            if (freq % 2 == 0) {
+                count = count + freq;
+            }
+
+            else {
+                count = count + freq - 1;
+                oddcount++;
+            }
+        }
+        if (oddcount > 0)
+            return count + 1;
+        return count;
     }
 }
