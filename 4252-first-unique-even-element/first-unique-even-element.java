7aef99bd83c9a33a3e1472084f1a4327a6ddaf62
shareefuddin886 <shareefkhaja974@gmail.com>
2026-09-09 12:03:41 +0530

    Time: 1 ms (98.95%) | Memory: 45.6 MB (80.23%) - LeetSync

--- 4252-first-unique-even-element/first-unique-even-element.java
+++ 4252-first-unique-even-element/first-unique-even-element.java
@@ -1,22 +1,18 @@
 class Solution {
     public int firstUniqueEven(int[] nums) {
-       HashMap<Integer,Integer> map=new HashMap<>();
+      int[] freq = new int[101];
        for(int i=0;i<nums.length;i++){
-        if(!map.containsKey(nums[i])){
-            map.put(nums[i],1);
-        }
-        else{
-            int freq=map.get(nums[i]);
-            map.put(nums[i],freq+1);
+        if(nums[i]%2==0){
+            freq[nums[i]]++;
         }
        }
 
        for(int i=0;i<nums.length;i++){
         if(nums[i]%2==0){
-            int ele=map.get(nums[i]);
-            if(ele==1) return nums[i];
+            if(freq[nums[i]]==1) return nums[i];
         }
        }
        return -1;
     }
 }
+
